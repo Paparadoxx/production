7b6e9a0db36e5f3c6c293ea71b03ba6bd9f189b1
Alexander Bolshakov <81434888+Paparadoxx@users.noreply.github.com>
2023-02-01 19:31:55 +0400

    route code splitting lazy

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,13 +1,17 @@
-import { BuildPaths, BuildOptions } from './types/config';
-import webpack from 'webpack';
-import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import webpack from 'webpack';
+import { BuildOptions } from './types/config';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
 	return [
 		new HtmlWebpackPlugin({
-			templaete: paths.html,
+			template: paths.html,
 		}),
 		new webpack.ProgressPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+		})
 	]
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
+import { Suspense } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+
 
 
 const App = () => {
 	return (
-		<div>App</div>
+		<div>
+			<Link to={'/'}>Главная</Link>
+			<Link to={'/about'}>О сайте</Link>
+			<Suspense fallback={<div>Загрузка...</div>}>
+				<Routes>
+					<Route path={'/about'} element={<AboutPageAsync/>}/>
+					<Route path={'/'} element={<MainPageAsync/>}/>
+				</Routes>
+			</Suspense>
+		</div>
 	)
 }
 
