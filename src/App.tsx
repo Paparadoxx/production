6667983b864b2eaca0a0adf676e308c692957cb1
Alexander Bolshakov <81434888+Paparadoxx@users.noreply.github.com>
2023-02-01 20:40:25 +0400

    added light and dark themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
+import './styles/index.scss';
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-
-
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+	const {theme, toggleTheme} = useTheme();
 	return (
-		<div>
+		<div className={`app ${theme}`}>
+			<button onClick={toggleTheme}>Изменить тему</button>
 			<Link to={'/'}>Главная</Link>
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Загрузка...</div>}>
