10d0f04156abb7656827fde36ddf11e98b2a1e0c
Alexander Bolshakov <81434888+Paparadoxx@users.noreply.github.com>
2023-02-03 16:08:00 +0400

    refactor with FSD architecture

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import './styles/index.scss';
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classnames/classNames";
+
+import { useTheme } from "./providers/ThemeProvider/lib/useTheme";
+import { classNames } from "../shared/lib/classnames/classNames";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 const App = () => {
 	const {theme, toggleTheme} = useTheme();
@@ -15,8 +16,8 @@ const App = () => {
 			<Link to={'/about'}>О сайте</Link>
 			<Suspense fallback={<div>Загрузка...</div>}>
 				<Routes>
-					<Route path={'/about'} element={<AboutPageAsync/>}/>
-					<Route path={'/'} element={<MainPageAsync/>}/>
+					<Route path={'/about'} element={<AboutPage/>}/>
+					<Route path={'/'} element={<MainPage/>}/>
 				</Routes>
 			</Suspense>
 		</div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 
+import { ThemeProvider } from 'App/providers/ThemeProvider';
 import {render} from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import ThemeProvider from './theme/ThemeProvider';
+import App from './App/App';
+
 
 render (
 	<BrowserRouter>
